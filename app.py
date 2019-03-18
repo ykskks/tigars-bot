4de4a4030c9bc89448d7c2edaca17278c0160556
ykskks <fkt.kohei@gmail.com>
2019-03-18 14:25:59 +0900

    bug fix

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ from linebot.models import (
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+channel_secret = os.getenv('YOUR_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    print('Specify YOUR_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
 if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    print('Specify YOUR_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
