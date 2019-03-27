92afa6774d3e35621dc334d90504b59c43dc48a8
ykskks <fkt.kohei@gmail.com>
2019-03-27 14:24:01 +0900

    add get_broadcaster_info

--- app.py
+++ app.py
@@ -13,20 +13,21 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('YOUR_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify YOUR_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify YOUR_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+# get your environment variables
+channel_secret = os.getenv('YOUR_CHANNEL_SECRET')
+channel_access_token = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
+
+time_zone = os.getenv('TZ')
 
 
 @app.route("/")
@@ -49,6 +50,52 @@ def callback():
         abort(400)
 
     return 'OK'
+
+
+def game_scheduled():
+    url = os.getenv('URL')
+    today = datetime.date.today(time_zone).strftime('%-m/%-d')
+    res = requests.get(url).text
+    soup = BeautifulSoup(res, 'html.parser')
+    #check if there is a match scheduled today and return True if so
+    for p in soup.find_all('p'):
+        if p.string == today:
+            return True
+            break
+    else:
+        return False
+
+def get_broadcast_info():
+    url = os.getenv('URL')
+    #get broadcasters table for today and extract broadcasters that is available for me
+    broadcasters = pd.read_html(url)[0]
+    broadcasters = broadcasters[(broadcasters['種別'] == '地上波') | (broadcasters['放送局'] == 'DAZN')].reset_index(drop=True)
+    return broadcasters
+
+
+@app.route("/push_broadcast_info", methods=['GET'])
+def push_broadcast_info():
+    today = datetime.date.today(time_zone).strftime('%-m/%-d')
+    
+    if game_scheduled():
+        broadcasters = get_broadcast_info()
+        #試合はあるが中継がないとき
+        if len(broadcasters) == 0:
+            push_text = today + 'の中継予定なし\n'
+        else:
+            push_text = today + 'の中継予定:\n'
+            for i in range(len(broadcasters)):
+                broadcaster_name = broadcasters.loc[i, '放送局']
+                broadcast_time = broadcasters.loc[i,  '時間']
+                push_text += broadcaster_name + ' ' + broadcast_time + '\n'
+    else:
+        push_text = today + 'の試合予定なし:\n'
+
+    to = os.getenv('YOUR_USER_ID')
+    line_bot_api.push_message(to, TextSendMessage(text=push_text))
+
+    return 'OK'
+
 
 
 @handler.add(MessageEvent, message=TextMessage)
